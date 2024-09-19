9040facf7f87a28db3b2d4b6c88ec358ea0e8f89
kayapse <157023812+kayapse@users.noreply.github.com>
2024-09-19 22:32:03 +0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 1;
+    int b = 50;
+    for(int i=1; i<50; i++){
+    if( i % 2 !=0) {    
+    cout<<i<< " ";
+    }
+    }
+}   
+
